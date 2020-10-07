4ed9392467116a6ea732a72ea844acc6a343f4a3
Lucy Dupree <lucybdupree@gmail.com>
2020-10-07 15:09:11 -0400

    Sale price showing

--- vue/src/components/ProductList.vue
+++ vue/src/components/ProductList.vue
@@ -5,33 +5,45 @@
     <input type="checkbox" v-model="filter.sale" />
     <label for="checkbox"> Sale Items Only</label>
     <div class="filter">
-  <label for="basic-dropdown">Categories: </label>
-  <select name="basic-dropdown" v-model="filter.categoryId">
-    <option value=""> View All </option>
-    <option value="1">frozen</option>
-    <option value="4">produce</option>
-    <option value="6">international</option>
-    <option value="7">beverages</option>
-    <option value="8">pets</option>
-    <option value="9">dry goods pasta</option>
-    <option value="11">personal care</option>
-    <option value="12">meat seafood</option>
-    <option value="13">pantry</option>
-    <option value="14">breakfast</option>
-    <option value="15">canned goods</option>
-    <option value="19">snacks</option>
-  </select>
-</div>
-    <div class="main">
-    <div class='container' v-for="product in filteredList" v-bind:key="product.id">
-      <div class="product-title">{{product.title}}</div>
-      <img class="thumbnail" v-if="product.image!=null" v-bind:src="product.image" />
-      <img class="thumbnail" v-else src='https://storage.needpix.com/rsynced_images/grocery-store-2119702_1280.jpg' />
-      <br>
-      <div v-if="product.sale != true">${{product.price}}.00</div>
-      <div v-else> {{product.discountedPrice}}</div>
-      {{product.weight}}oz
+      <label for="basic-dropdown">Categories: </label>
+      <select name="basic-dropdown" v-model="filter.categoryId">
+        <option value="">View All</option>
+        <option value="1">frozen</option>
+        <option value="4">produce</option>
+        <option value="6">international</option>
+        <option value="7">beverages</option>
+        <option value="8">pets</option>
+        <option value="9">dry goods pasta</option>
+        <option value="11">personal care</option>
+        <option value="12">meat seafood</option>
+        <option value="13">pantry</option>
+        <option value="14">breakfast</option>
+        <option value="15">canned goods</option>
+        <option value="19">snacks</option>
+      </select>
     </div>
+    <div class="main">
+      <div
+        class="container"
+        v-for="product in filteredList"
+        v-bind:key="product.id"
+      >
+        <div class="product-title">{{ product.title }}</div>
+        <img
+          class="thumbnail"
+          v-if="product.image != null"
+          v-bind:src="product.image"
+        />
+        <img
+          class="thumbnail"
+          v-else
+          src="https://storage.needpix.com/rsynced_images/grocery-store-2119702_1280.jpg"
+        />
+        <br />
+        <div v-if="product.sale != true">${{ product.price }}.00</div>
+        <div v-else>was ${{product.price}}.00 now ${{ product.discountedPrice }}0</div>
+        {{ product.weight }}oz
+      </div>
     </div>
   </div>
 </template>
@@ -39,51 +51,55 @@
 <script>
 import productService from "../services/ProductService.js";
 export default {
-    data() {
-      return {
-        products: [],
-        filter: {
-          title:"",
-          sale: false,
-          categoryId:""
-        },
-      }
-    },
-    methods: {
-     findSalePrice() {
+  data() {
+    return {
+      products: [],
+      filter: {
+        title: "",
+        sale: false,
+        categoryId: "",
+      },
+    };
+  },
+  methods: {
+    findSalePrice() {
       if (this.product.sale) {
-        this.product.price = this.product.price * 0.90
+        this.product.price = this.product.price * 0.9;
       }
-      console.log(this.product.price)
-      return this.product.price
-     }
+      console.log(this.product.price);
+      return this.product.price;
     },
-    computed: {
-filteredList() {
-let filteredProducts = this.products;
-if (this.filter.title != "") {
-filteredProducts = filteredProducts.filter((item) =>
-item.title.toLowerCase().includes(this.filter.title.toLowerCase()));
-}
-if (this.filter.sale != "") {
-filteredProducts = filteredProducts.filter((item) =>
-item.sale === this.filter.sale
-);
-}
-if (this.filter.categoryId != "") {
-  filteredProducts = filteredProducts.filter((item) =>
-item.categoryId == this.filter.categoryId
-);
-}
-return filteredProducts;
-},
-},
-    created() {
+  },
+  computed: {
+    filteredList() {
+      let filteredProducts = this.products;
+      if (this.filter.title != "") {
+        filteredProducts = filteredProducts.filter((item) =>
+          item.title.toLowerCase().includes(this.filter.title.toLowerCase())
+        );
+      }
+      if (this.filter.sale != "") {
+        filteredProducts = filteredProducts.filter(
+          (item) => item.sale === this.filter.sale
+        );
+      }
+      if (this.filter.categoryId != "") {
+        filteredProducts = filteredProducts.filter(
+          (item) => item.categoryId == this.filter.categoryId
+        );
+      }
+      return filteredProducts;
+    },
+  },
+  created() {
     productService.getAllProducts().then((response) => {
       this.products = response.data;
-      this.products.forEach((product) => {product.discountedPrice = product.price * 0.9})
+      this.products.forEach((product) => {
+        product.discountedPrice = product.price * 0.9;
+      });
     });
-  }}
+  },
+};
 </script>
 
 <style>
