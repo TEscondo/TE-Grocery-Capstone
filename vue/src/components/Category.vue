ea8e0da846a0644927a7eb0757b2107c4722a9e3
Jacob Habib <habib.50@osu.edu>
2020-10-08 16:36:12 -0400

    Titles per category created

--- vue/src/components/Category.vue
+++ vue/src/components/Category.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="product-by-category">
     <div class="category-title">
-      <h1>categoryName</h1>
+      <h1>{{returnCategoryName}}</h1>
     </div>
-    <p class="category-description">Description Here</p>
+    <p class="category-description">Category Description</p>
     <div class="main">
       <div
         class="container"
@@ -37,6 +37,74 @@
 <script>
 import productService from "../services/ProductService.js";
 export default {
+ computed: {
+    returnCategoryName() {
+        if (this.$route.params.categoryId == 1) {
+            return "Frozen";
+        }
+        if (this.$route.params.categoryId == 2) {
+            return "Other";
+        }
+        if (this.$route.params.categoryId == 3) {
+            return "Bakery";
+        }
+        if (this.$route.params.categoryId == 4) {
+            return "Produce";
+        }
+        if (this.$route.params.categoryId == 5) {
+            return "Alcohol";
+        }
+        if (this.$route.params.categoryId == 6) {
+            return "International";
+        }
+        if (this.$route.params.categoryId == 7) {
+            return "Beverages";
+        }
+        if (this.$route.params.categoryId == 8) {
+             return "Pets";
+        }
+        if (this.$route.params.categoryId == 9) {
+            return "Dry Goods Pasta";
+        }
+        if (this.$route.params.categoryId == 10) {
+            return "Bulk";
+        }
+        if (this.$route.params.categoryId == 11) {
+            return "Personal Care";
+        }
+        if (this.$route.params.categoryId == 12) {
+            return "Meat & Seafood";
+        }
+        if (this.$route.params.categoryId == 13) {
+            return "Pantry";
+        }
+        if (this.$route.params.categoryId == 14) {
+            return "Breakfast";
+        }
+        if (this.$route.params.categoryId == 15) {
+            return "Canned Goods";
+        }
+        if (this.$route.params.categoryId == 16) {
+            return "Dairy Eggs";
+        }
+        if (this.$route.params.categoryId == 17) {
+            return "Household";
+        }
+        if (this.$route.params.categoryId == 18) {
+            return "Babies";
+        }
+        if (this.$route.params.categoryId == 19) {
+            return "Snacks";
+        }
+        if (this.$route.params.categoryId == 20) {
+            return "Deli";
+        }
+        if (this.$route.params.categoryId == 21) {
+            return "Missing";
+        }
+        return "Other";
+    }
+ },
   data() {
     return {
       products: [],
